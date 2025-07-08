7d7aa9a82ebbda5208788dbd509120a60b9640da
Rushi5078 <rushikeshb5078@gmail.com>
2025-07-08 15:10:55 +0530

    Update the sum.js

--- sum.js
+++ sum.js
@@ -1,4 +1,12 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const result = 2 + 3;
+  res.end(`The result of 2 + 3 is ${result}`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
